1ccaece13022b9174d2ede823480b04cc74d4a22
likogan <me@likogan.dev>
2023-07-17 23:29:56 -0600

    Add volume up/down & media pause keys

--- config.h
+++ config.h
@@ -60,12 +60,16 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *lockcmd[]  = { "slock", NULL };
-static const char *volup[]    = { "pactl", "set-sink-volume 0 +5%", NULL };
-static const char *voldown[]  = { "pactl", "set-sink-volume", "0", "-5%", NULL };
+static const char *dmenucmd[]  = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+// Apps
+static const char *termcmd[]   = { "st", NULL };
+static const char *lockcmd[]   = { "slock", NULL };
+// Commands */
+static const char *volup[]     = { "pactl", "set-sink-volume", "0", "+5%",       NULL };
+static const char *voldown[]   = { "pactl", "set-sink-volume", "0", "-5%", NULL };
+static const char *playpause[] = { "playerctl", "play-pause",              NULL };
 
+#include <X11/XF86keysym.h>
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -77,8 +81,8 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_k,      focusstackhid,  {.i = -1 } },
 	{ MODKEY,                       XK_Escape, spawn,          { .v = lockcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+//	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+//	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
@@ -103,6 +107,11 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	// XF86XK keys                  key                        function        argument
+	{ 0,                            XF86XK_AudioRaiseVolume,   spawn,        {.v = volup     } },
+	{ 0,                            XF86XK_AudioLowerVolume,   spawn,        {.v = voldown   } },
+	{ 0,                            XF86XK_AudioPlay,          spawn,        {.v = playpause } },
+	// Tag keys */	
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
